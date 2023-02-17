f8124c6b3fb2c9fdd8c588b33227f3527ef0b79e
Pier Bomarshegit config --global user.email pezh2008@gmail.com <84318712+pezh2008@users.noreply.github.com>
2023-02-17 17:06:37 +0300

    test5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-const slider = new Swiper('.slider', {
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+
+
+burger.addEventListener("click",()=>{
+ menu.classList.toggle("toggle-menu");
+ burger.classList.toggle("toggle-burger");
+})
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Navigation arrows
+  speed: 2400,
   
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      allowTouchMove: true,
+    },
+
+    768: {
+      allowTouchMove: false,
+    }
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
