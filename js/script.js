e6066a8baf9cbf2d5a49660d0ae0faa0b576b485
Pier Bomarshegit config --global user.email pezh2008@gmail.com <84318712+pezh2008@users.noreply.github.com>
2023-02-22 18:36:12 +0300

    test-99

--- js/script.js
+++ js/script.js
@@ -31,3 +31,37 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+const bar = document.querySelector(".interest__bar");
+const interest = document.querySelector(".interest");
+const num = document.querySelector(".interest__number");
+let count = 0;
+
+
+function counterAndBgColor() {
+  bar.style.height = "100%";
+  let interval = setInterval(() => {
+    count = count + 1;
+    if (count >= 50) {
+      clearInterval(interval);
+    }
+    num.innerHTML = `+${count}`;
+    
+  }, 70);
+}
+
+let observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        counterAndBgColor()
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.8,
+  }
+);
+observer.observe(interest);
